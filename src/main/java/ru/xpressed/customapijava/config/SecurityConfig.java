c1a7e4658c96c4228e13aea9f39befd6f694795d
EZzic <max.zvyagincev@mail.ru>
2022-09-27 23:45:02 +0300

    OpenAPI update

--- src/main/java/ru/xpressed/customapijava/config/SecurityConfig.java
+++ src/main/java/ru/xpressed/customapijava/config/SecurityConfig.java
@@ -1,17 +1,17 @@
-package ru.xpressed.customapijava.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    @Override
-    public void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests().anyRequest().permitAll()
-                .and().cors().and().csrf().disable();
-    }
-}
+//package ru.xpressed.customapijava.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//    @Override
+//    public void configure(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeRequests().anyRequest().permitAll()
+//                .and().cors().and().csrf().disable();
+//    }
+//}
